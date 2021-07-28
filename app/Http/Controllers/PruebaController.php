6e670bee99a4a07dbe3f9e5d263b2e32165923a5
Heber Bastidas <heber.bastidas@rentger.com>
2021-07-28 22:47:31 +0200

    enable views over controller.. move welcome page to controller action welcome

--- app/Http/Controllers/PruebaController.php
+++ app/Http/Controllers/PruebaController.php
@@ -7,4 +7,13 @@ use Illuminate\Support\Facades\Http;
 
 class PruebaController extends Controller
 {
+  /**
+   * Show the my users page.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function welcome()
+  {
+      return view('welcome');
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PruebaController@welcome');
+//a laravel welcome initial app
+Route::get('/welcome', 'PruebaController@welcome');
