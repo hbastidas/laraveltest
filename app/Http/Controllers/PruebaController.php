efffb202ead12f47fab4c84d93e2bf5bd4901791
Heber Bastidas <heber.bastidas@rentger.com>
2021-07-28 22:57:20 +0200

    new base view and controller for laraveltest

--- app/Http/Controllers/PruebaController.php
+++ app/Http/Controllers/PruebaController.php
@@ -8,6 +8,18 @@ use Illuminate\Support\Facades\Http;
 class PruebaController extends Controller
 {
   /**
+   * Show the my laraveltest page form
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function laraveltest()
+  {
+      
+      $data=array();
+      return view('laraveltest',$data);
+  }
+
+  /**
    * Show the my users page.
    *
    * @return \Illuminate\Http\Response
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'PruebaController@welcome');
+//a view to test form
+Route::get('/laraveltest', 'PruebaController@laraveltest');
 //a laravel welcome initial app
 Route::get('/welcome', 'PruebaController@welcome');
